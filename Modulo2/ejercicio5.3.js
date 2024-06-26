f5a8b33800b8b981af8c2cb9c9dd64124c0f9a52
soft_dev_hugo_ <A01228679@exatec.tec.mx>
2024-06-25 21:47:40 -0600

    Create ejercicio5.3.js

---
+++ Modulo2/ejercicio5.3.js
@@ -0,0 +1,20 @@
+let numeros = [5, 12, 8, 7, 9, 3, 10, 2, 6];
+
+// Imprimir el tercer elemento del array
+console.log(numero[2])
+
+// Eliminar el quinto elemento del array
+numeros.splice(4, 1);
+
+// Imprimir el arreglo después de la eliminación
+console.log(numeros);
+
+// Eliminar el primer elemento
+numeros.shift();
+
+// Imprimir el arreglo una vez más
+console.log(numeros);
+
+// Agregar 1 elemento aleatorio al final y uno al inicio
+numeros.push(4);
+numeros.unshift(1);

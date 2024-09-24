559c582ec4373afe59e03f3f2a1eff437e5300b7
soft_dev_hugo_ <A01228679@exatec.tec.mx>
2024-09-23 18:12:54 -0600

    Adding component functionality

--- SmartShopsHugo/app/src/main/java/com/example/smartshophugo/MainActivity.kt
+++ SmartShopsHugo/app/src/main/java/com/example/smartshophugo/MainActivity.kt
@@ -1,13 +1,40 @@
 package com.example.smartshophugo
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var etUsername: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var btnLogin: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        etUsername = findViewById(R.id.etUsername)
+        etPassword = findViewById(R.id.etPassword)
+        btnLogin = findViewById(R.id.btnLogin)
+
+        btnLogin.setOnClickListener{
+            val username = etUsername.text.toString()
+            val password = etPassword.text.toString()
+            login(username, password)
+        }
     }
+
+    fun login( username: String, password: String){
+        if (username.isNotEmpty() && password.isNotEmpty()) {
+            Toast.makeText(this, "Login exitoso", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Login fallido", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
